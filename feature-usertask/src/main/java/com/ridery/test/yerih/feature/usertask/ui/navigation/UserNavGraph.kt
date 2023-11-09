c4a4f7b1426c2e279cd37056dab8ac5cb3454741
yerih <yerih7tup@gmail.com>
2023-11-09 19:32:46 -0400

    Project built. NavGraph set. Splash screen created.

---
+++ feature-usertask/src/main/java/com/ridery/test/yerih/feature/usertask/ui/navigation/UserNavGraph.kt
@@ -0,0 +1,37 @@
+package com.ridery.test.yerih.feature.usertask.ui.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.ridery.test.yerih.feature.usertask.ui.UserTaskScreen
+
+
+fun NavGraphBuilder.userNavGraph(navController: NavController){
+    val signinRoute = "main/sign_in"
+    val signupRoute = "main/sign_up"
+    val homeRoute = "main/home"
+    val routeGraph = "main"
+    navigation(
+        route = routeGraph,
+        startDestination = signinRoute,
+    ){
+        composable(route = signinRoute){
+            UserTaskScreen(modifier = Modifier.padding(16.dp))
+        }
+
+        composable(route = signupRoute){
+            UserTaskScreen(modifier = Modifier.padding(16.dp))
+        }
+
+        composable(route = homeRoute){
+            UserTaskScreen(modifier = Modifier.padding(16.dp))
+        }
+    }
+
+}
+
+
