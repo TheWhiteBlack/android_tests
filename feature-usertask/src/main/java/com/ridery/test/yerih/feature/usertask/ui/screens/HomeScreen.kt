65c9b5d0cd75c8eec2082c58c0a1c98f3f10bf08
yerih <yerih7tup@gmail.com>
2023-11-11 08:05:25 -0400

    Swipeworking

--- feature-usertask/src/main/java/com/ridery/test/yerih/feature/usertask/ui/screens/HomeScreen.kt
+++ feature-usertask/src/main/java/com/ridery/test/yerih/feature/usertask/ui/screens/HomeScreen.kt
@@ -1,11 +1,15 @@
 package com.ridery.test.yerih.feature.usertask.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
@@ -27,7 +32,6 @@ import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
-import com.ridery.test.yerih.core.domain.UserDomain
 import com.ridery.test.yerih.core.ui.Font
 import kotlinx.coroutines.delay
 
@@ -39,24 +43,24 @@ fun HomeScreen(
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(singapore, 10f)
     }
-    var isRefreshing by remember { mutableStateOf(true) }
+    var refreshing by remember { mutableStateOf(false) }
 
-    LaunchedEffect(key1 = isRefreshing){
-        if(isRefreshing){
-            delay(2500)
-            isRefreshing = false
+    LaunchedEffect(key1 = refreshing) {
+        if (refreshing) {
+            delay(1500)
+            refreshing = false
         }
     }
-    
+
+
     SwipeRefresh(
-        state = rememberSwipeRefreshState(isRefreshing = isRefreshing),
-        modifier = Modifier.fillMaxSize(),
-        onRefresh = { isRefreshing = true },
+        state = rememberSwipeRefreshState(isRefreshing = refreshing),
+        onRefresh = { refreshing = true },
     ) {
 
         ConstraintLayout(
-            modifier = Modifier
-                .padding(horizontal = 30.dp),
+            modifier = Modifier.fillMaxSize().padding(horizontal = 30.dp)
+                .verticalScroll(rememberScrollState()),
         ) {
             val (welcome, map) = createRefs()
             Text(
@@ -89,7 +93,10 @@ fun HomeScreen(
                 }
 
             }
+
         }
     }
+
+
 
 }
